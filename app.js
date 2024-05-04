782ef4442175e186877667629bcf0f8ceea708d4
Abeer Iqbal <abeeriqbalahmed@gmail.com>
2024-05-04 20:15:43 +0500

    updated now 8;15pm

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Slider//
+var sliderCount = 0;
 var imgDiv = document.getElementById("imgDiv");
 var arrImg = imgDiv.getElementsByTagName("IMG");
 var srcOfImg = [];
@@ -10,9 +11,28 @@ for (var i = 0; i < arrImg.length; i++) {
 
 var mainImgDiv = document.getElementById("mainImg");
 function renderSlide() {
-        if (srcOfImg.length > 0) {
-                mainImg.src = srcOfImg[0];
-            }
+        // if (srcOfImg.length > 0) {
+        //         mainImg.src = srcOfImg[0];
+        //     }
+        imgDiv.src = srcOfImg[sliderCount];
         }
 
+renderSlide()
+
+function nextSlide() {
+        if (sliderCount == srcOfImg.length -1) {
+                sliderCount = 0
+        } else {
+                sliderCount ++
+        }
+}       
+ renderSlide()
+ 
+ function prevSlide() {
+         if (sliderCount == srcOfImg.length -1) {
+                 sliderCount = 0
+                } else {
+                        sliderCount --
+                }
+        } 
 renderSlide()
