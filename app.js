fb228ea10df14867b68c115577e0bd48a71ae0f7
Abeer Iqbal <abeeriqbalahmed@gmail.com>
2024-05-06 13:02:27 +0500

    updated slides code

--- app.js
+++ app.js
@@ -3,36 +3,40 @@ var sliderCount = 0;
 var imgDiv = document.getElementById("imgDiv");
 var arrImg = imgDiv.getElementsByTagName("IMG");
 var srcOfImg = [];
+var mainImgDiv = document.getElementById("mainImg");
 
 for (var i = 0; i < arrImg.length; i++) {
         srcOfImg.push(arrImg[i].src);
 }
         console.log(srcOfImg); 
 
-var mainImgDiv = document.getElementById("mainImg");
 function renderSlide() {
         // if (srcOfImg.length > 0) {
         //         mainImg.src = srcOfImg[0];
         //     }
-        imgDiv.src = srcOfImg[sliderCount];
-        }
+        mainImg.src = srcOfImg[sliderCount];
+}
 
-renderSlide()
+renderSlide();
 
 function nextSlide() {
         if (sliderCount == srcOfImg.length -1) {
-                sliderCount = 0
+                sliderCount = 0;
+                console.log(sliderCount)
         } else {
-                sliderCount ++
+                sliderCount ++;
+                console.log(sliderCount)
         }
+        renderSlide()
 }       
- renderSlide()
- 
- function prevSlide() {
-         if (sliderCount == srcOfImg.length -1) {
-                 sliderCount = 0
-                } else {
-                        sliderCount --
-                }
+
+function prevSlide() {
+        if (sliderCount == srcOfImg.length -1) {
+                sliderCount = 0;
+                console.log(sliderCount)
+        } else {
+                sliderCount --;
+                console.log(sliderCount)
+        }
+        renderSlide()
         } 
-renderSlide()
